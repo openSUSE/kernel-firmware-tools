3ca3391739aa1d98f7d283855862e7089e8c8653
Takashi Iwai <tiwai@suse.de>
2026-06-29 15:20:02 +0200

    kernel-firmware.spec: install extra documents, too

--- scripts/strip-topic-whence.sh
+++ scripts/strip-topic-whence.sh
@@ -3,11 +3,16 @@
 #
 # Strip WHENCE only for the given topic
 #
-# usage: strip-topic-whence.sh $TOPIC < WHENCE
+# usage: strip-topic-whence.sh [-t DOCDIR] $TOPIC < WHENCE
+#   -t = install the documents like README to the given directory
 #
 
+if [ x"$1" = x"-t" ]; then
+    text_target=$2
+    shift 2
+fi
+
 chosen="$1"
-list="$2"
 
 declare -A topicdefs
 
@@ -20,6 +25,15 @@ while read drv t mods; do
     esac
 done < topics.list
 
+install_text () {
+    test -n "$text_target" || return
+    local l="$1"
+    l="${l#File: }"
+    test -f $l || return
+    local d=$(echo $l | tr / -)
+    install -c -m 0644 $l "$text_target/$d"
+}
+
 sub="xxx"
 while IFS="" read -r l; do
     case "$l" in
@@ -28,24 +42,22 @@ while IFS="" read -r l; do
 	    topic=""
 	    continue
 	    ;;
-	File:\ */README)
-	    continue
-	    ;;
-	File:\ */notice_ath*.txt)
-	    continue
-	    ;;
-	File:\ */notice.txt_wlanmdsp)
-	    continue
-	    ;;
-	File:\ */Notice.txt)
-	    continue
-	    ;;
-	File:\ */.notice)
-	    continue
-	    ;;
     esac
     if [ "$topic" = "$chosen" -o "$sub" = "xxx" ]; then
-	echo "$l"
+	case "$l" in
+	    File:\ */README*)
+		install_text "$l";;
+	    File:\ */notice_ath*.txt)
+		install_text "$l";;
+	    File:\ */notice.txt_wlanmdsp)
+		install_text "$l";;
+	    File:\ */Notice.txt)
+		install_text "$l";;
+	    File:\ */.notice)
+		install_text "$l";;
+	    *)
+		test -z "$text_target" && echo "$l" ;;
+	esac
 	continue
     fi
     case "$l" in
@@ -54,6 +66,7 @@ while IFS="" read -r l; do
 	    sub="$1"
 	    sub="${sub%:}"
 	    topic="${topicdefs[$sub]}"
+	    test -n "$text_target" && continue
 	    if [ "$topic" = "$chosen" ]; then
 		echo "--------------------------------------------------------------------------"
 		echo
